62c9c5f7089b834fc182a0c6130b8cd71699ed5b
CypherPoet <CypherPoet@gmail.com>
2019-09-11 00:11:12 -0400

    Implement CircleImageView

--- Landmarks/Landmarks/Scenes/ContentView.swift
+++ Landmarks/Landmarks/Scenes/ContentView.swift
@@ -25,7 +25,6 @@ struct ContentView: View {
             }
         }
         .padding()
-        
     }
 }
 
---
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Brian Sipple on 9/10/19.
+//  Copyright © 2019 CypherPoet. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
