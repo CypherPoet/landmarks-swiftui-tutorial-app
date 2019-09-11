a2d4e32a046b7d6197bf661c2a65483fe7e6d872
CypherPoet <CypherPoet@gmail.com>
2019-09-11 00:34:34 -0400

    Implement MKMapView wrapped in a SwiftUI View.

---
+++ Landmarks/Landmarks/Views/MapView.swift
@@ -0,0 +1,42 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Brian Sipple on 9/11/19.
+//  Copyright © 2019 CypherPoet. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct MapView: View {
+}
+
+
+extension MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    
+    func updateUIView(_ mapView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286,
+            longitude: -116.166868
+        )
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
